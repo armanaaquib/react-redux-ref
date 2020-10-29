6be4c2090b3f4343e95a48322657f4287aa95fb6
Aaquib Equbal <armanaaquibequbal@gmail.com>
2020-10-29 07:48:31 +0530

    Refactor TodoList: Pass todo id in toggleTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 import Todo from './Todo';
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({ todos, toggleTodo }) => (
   <ul>
     {todos.map((todo) => (
-      <Todo key={todo.id} {...todo} onClick={onTodoClick} />
+      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
     ))}
   </ul>
 );
@@ -19,7 +19,7 @@ TodoList.propTypes = {
       text: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
-  onTodoClick: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
 };
 
 export default TodoList;
--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -20,7 +20,7 @@ describe('TodoList', () => {
   ];
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<TodoList todos={todos} onTodoClick={jest.fn()} />);
+    const wrapper = shallow(<TodoList todos={todos} toggleTodo={jest.fn()} />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
@@ -28,12 +28,25 @@ describe('TodoList', () => {
   it('should pass all required props to Todo', () => {
     const mockOnTodoClick = jest.fn();
     const wrapper = mount(
-      <TodoList todos={todos} onTodoClick={mockOnTodoClick} />
+      <TodoList todos={todos} toggleTodo={mockOnTodoClick} />
     );
 
-    expect(wrapper.find(Todo).first().props()).toStrictEqual({
-      ...todos[0],
-      onClick: mockOnTodoClick,
+    const props = wrapper.find(Todo).first().props();
+
+    expect(props).toHaveProperty('id', 1);
+    expect(props).toHaveProperty('completed', true);
+    expect(props).toHaveProperty('text', 'Consider using Redux');
+  });
+
+  it('should call togllTodo with todo id', () => {
+    const mockOnTodoClick = jest.fn((id) => {
+      expect(id).toBe(1);
     });
+
+    const wrapper = shallow(
+      <TodoList todos={todos} toggleTodo={mockOnTodoClick} />
+    );
+
+    wrapper.find(Todo).first().simulate('click');
   });
 });
